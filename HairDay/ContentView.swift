54946c17a1bc799174d38ac5d0ecdea47aead130
adamq1337 <adam.kajp@gmail.com>
2024-08-01 17:39:26 +0200

    Initial Commit

---
+++ HairDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HairDay
+//
+//  Created by Adam Persson on 2024-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HairDay/HairDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HairDayApp.swift
+//  HairDay
+//
+//  Created by Adam Persson on 2024-08-01.
+//
+
+import SwiftUI
+
+@main
+struct HairDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
